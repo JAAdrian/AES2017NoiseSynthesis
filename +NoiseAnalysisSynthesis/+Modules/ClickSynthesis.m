9ab3657bd7eacf8ebad944782318834cf5a240f7
Jens-Alrik Adrian <jens-alrik.adrian@gmx.de>
2017-10-03 22:03:37 +0200

    Remove and move files

---
+++ +NoiseAnalysisSynthesis/+Modules/ClickSynthesis.m
@@ -0,0 +1,67 @@
+classdef ClickSynthesis < matlab.System
+%CLICKSYNTHESIS <purpose in one line!>
+% -------------------------------------------------------------------------
+% <Detailed description of the function>
+%
+% ClickSynthesis Properties:
+%	propA - <description>
+%	propB - <description>
+%
+% ClickSynthesis Methods:
+%	doThis - <description>
+%	doThat - <description>
+%
+% Author :  J.-A. Adrian (JA) <jensalrik.adrian AT gmail.com>
+% Date   :  03-Oct-2017 21:51:00
+%
+
+% History:  v0.1.0   initial version, 03-Oct-2017 (JA)
+%
+
+
+properties (Access = public)
+	
+end
+
+properties (SetAccess = protected, GetAccess = public)
+	
+end
+
+properties (Access = protected)
+	
+end
+
+
+
+methods
+	function [obj] = ClickSynthesis(varargin)
+		obj.setProperties(nargin, varargin{:})
+	end
+end
+
+
+methods (Access = protected)
+	function [] = setupImpl(obj)
+		
+	end
+
+	function [] = resetImpl(obj)
+		
+	end
+
+	function [] = stepImpl(obj)
+		
+	end
+
+	function [] = releaseImpl(obj)
+		
+	end
+end
+
+end
+
+
+
+
+
+% End of file: ClickSynthesis.m

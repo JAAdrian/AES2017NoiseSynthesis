ee42cfce1e7ba1b224b4ac27f0e7c5c3cb0f44e2
Jens-Alrik Adrian <jens-alrik.adrian@gmx.de>
2018-02-11 16:05:59 +0100

    Refactor synthesis integration test

---
+++ +NoiseAnalysisSynthesis/+Tester/NoiseSynthesis_test.m
@@ -0,0 +1,64 @@
+classdef NoiseSynthesis_test < NoiseAnalysisSynthesis.Tester.NoiseSynthesisUnitTest
+%NOISESYNTHESIS_TEST Unit test for NoiseSynthesis.m
+% -------------------------------------------------------------------------
+% Run it by calling 'runtests()'
+%   or specifically 'runtests('NoiseSynthesis_test')'
+%
+% Author :  J.-A. Adrian (JA) <jensalrik.adrian AT gmail.com>
+% Date   :  11-Feb-2018 15:58:14
+%
+
+
+properties
+    UnitName = 'NoiseAnalysisSynthesis.Modules.NoiseSynthesis';
+    NoiseProperties;
+end
+
+
+methods (TestClassSetup)
+    function setupProperties(testCase)
+        noiseProperties = load('noiseprops.mat');
+        
+        testCase.NoiseProperties = noiseProperties;
+    end
+end
+
+methods (TestMethodSetup)
+    function setup(testCase)
+        noiseProperties = testCase.NoiseProperties.noiseProperties;
+        
+        synthesis = NoiseAnalysisSynthesis.Modules.NoiseSynthesis();
+        synthesis.NoiseProperties = noiseProperties;
+        
+        blocklenSamples = 1024;
+        sampleRate = 44.1e3;
+        synthesis.StftParameters = NoiseAnalysisSynthesis.STFTparams(...
+            blocklenSamples / sampleRate, ...
+            0.5, ...
+            sampleRate, ...
+            'synthesis' ...
+            );
+        
+        synthesis.DesiredLengthSignalSamples = synthesis.SampleRate;
+        
+        testCase.Unit = synthesis;
+        
+%         numBlocks = 1;
+%         blockedSignal = zeros(synthesis.StftParameters.Blocklen, numBlocks);
+%         for iBlock = 1:numBlocks
+%             blockedSignal(:, iBlock) = synthesis();
+%         end
+    end
+end
+
+
+
+methods (Test)
+    function runSynthesis(testCase)
+        testCase.Unit();
+    end
+end
+
+end
+
+% End of file: NoiseSynthesis_test.m
